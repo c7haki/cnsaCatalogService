57332dd6fa357556e772dd3b39e18e01b876a0a1
Markus Bösling <markus.boesling@telekom.de>
2023-08-15 13:24:55 +0200

    use explicit mapping function and fix return types

--- src/main/kotlin/de/yehaw/cnsacatalogservice/adapter/in/web/BookController.kt
+++ src/main/kotlin/de/yehaw/cnsacatalogservice/adapter/in/web/BookController.kt
@@ -14,41 +14,38 @@ import org.springframework.web.bind.annotation.*
 @RequestMapping("/api/v1/books")
 class BookController(
     private val bookService: BookService
-){
+) {
 
     @GetMapping
-    fun get(): Iterable<BookDto> = bookService.getBooks().map { BookDto(it) }
+    fun get(): Iterable<BookDto> = bookService.getBooks().map { it.toBookDto() }
 
     @GetMapping("{isbn}")
-    fun getByIsbn(@PathVariable isbn: String): BookDto = BookDto(bookService.getBook(isbn))
+    fun getByIsbn(@PathVariable isbn: String): BookDto = bookService.getBook(isbn).toBookDto()
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    fun post(@Valid @RequestBody book: BookDto) = bookService.addBook(book.toBook())
+    fun post(@Valid @RequestBody book: BookDto): BookDto = bookService.addBook(book.toBook()).toBookDto()
 
     @DeleteMapping("{isbn}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    fun delete(@PathVariable isbn: String) = bookService.removeBook(isbn)
+    fun delete(@PathVariable isbn: String): Unit = bookService.removeBook(isbn)
 
     @PutMapping("{isbn}")
-    fun put(@PathVariable isbn: String, @Valid @RequestBody book: BookDto) = bookService.editBook(isbn, book.toBook())
+    fun put(@PathVariable isbn: String, @Valid @RequestBody book: BookDto): BookDto =
+        bookService.editBook(isbn, book.toBook()).toBookDto()
 
 }
 
 data class BookDto(
-    @get:NotBlank(message = "The book ISBN must not be blank.")
-    @get:Pattern(regexp = "^[0-9]{10}|[0-9]{13}$", message = "The book ISBN format is invalid.")
-    val isbn: String,
-    @get:NotBlank(message = "The book title must not be blank.")
-    val title: String,
-    @get:NotBlank(message = "The book author must not be blank.")
-    val author: String,
-    @get:NotNull(message = "The book price must be defined.")
-    @get:Positive(message = "The book price must be greater than zero.")
-    val price: Double,
-    @get:NotBlank(message = "The publisher must not be blank.")
-    val publisher: String,
-    ) {
-    constructor(book: Book): this(book.isbn, book.title, book.author, book.price, book.publisher)
+    @get:NotBlank(message = "The book ISBN must not be blank.") @get:Pattern(
+        regexp = "^[0-9]{10}|[0-9]{13}$", message = "The book ISBN format is invalid."
+    ) val isbn: String,
+    @get:NotBlank(message = "The book title must not be blank.") val title: String,
+    @get:NotBlank(message = "The book author must not be blank.") val author: String,
+    @get:NotNull(message = "The book price must be defined.") @get:Positive(message = "The book price must be greater than zero.") val price: Double,
+    @get:NotBlank(message = "The publisher must not be blank.") val publisher: String,
+) {
     fun toBook() = Book(isbn, title, author, price, publisher)
 }
+
+fun Book.toBookDto() = BookDto(this.isbn, this.title, this.author, this.price, this.publisher)
